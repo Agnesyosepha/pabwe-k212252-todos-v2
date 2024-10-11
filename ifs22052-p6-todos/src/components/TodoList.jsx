f80593b05976f61960ca3e5569068b7891757559
Agnesyosepha <149126712+Agnesyosepha@users.noreply.github.com>
2024-10-11 08:43:11 +0700

    TodoList.jsx

--- ifs22052-p6-todos/src/components/TodoList.jsx
+++ ifs22052-p6-todos/src/components/TodoList.jsx
@@ -1,16 +1,36 @@
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import TodoItem, { todoItemShape } from "./TodoItem";
+import TodoDetail from "./TodoDetail";
+
 function TodoList({ todos, onDeleteTodo }) {
+  const [todoList, setTodoList] = useState(todos);
+
+  // Fungsi untuk memperbarui todo
+  const handleUpdateTodo = (updatedTodo) => {
+    const updatedTodos = todoList.map((todo) =>
+      todo.id === updatedTodo.id ? updatedTodo : todo
+    );
+    setTodoList(updatedTodos); // Perbarui state todoList
+  };
+
   return (
     <div>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onDeleteTodo={onDeleteTodo} />
+      {todoList.map((todo) => (
+        <TodoDetail
+          key={todo.id}
+          todo={todo}
+          onUpdate={handleUpdateTodo}
+          onDeleteTodo={onDeleteTodo}
+        />
       ))}
     </div>
   );
 }
+
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.shape(todoItemShape)).isRequired,
   onDeleteTodo: PropTypes.func.isRequired,
 };
+
 export default TodoList;
